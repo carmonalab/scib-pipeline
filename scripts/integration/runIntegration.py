fa5fad5a0560c76be5f70d42791f6c97c5576575
leonardHerault <leo.herault9338@gmail.com>
2023-04-26 11:59:55 +0200

    adding new scgen and scanorama functions to make integration with a latent space of 30 components (overidding scib.integration functions)

--- scripts/integration/runIntegration.py
+++ scripts/integration/runIntegration.py
@@ -7,6 +7,66 @@ import warnings
 
 warnings.filterwarnings('ignore')
 
+def scgen(adata, batch, cell_type, epochs=100, latent_dims = 30, hvg=None, **kwargs):
+    """scGen wrapper function
+    Based on `scgen`_ version 2.1.0 with parametrization taken from the tutorial `notebook`_.
+    with adapted default latent space size
+    .. _scgen: https://github.com/theislab/scgen
+    .. _notebook: https://scgen.readthedocs.io/en/stable/tutorials/scgen_batch_removal.html
+    :param adata: preprocessed ``anndata`` object
+    :param batch: batch key in ``adata.obs``
+    :param hvg: list of highly variables to subset to. If ``None``, the full dataset will be used
+    :return: ``anndata`` object containing the corrected feature matrix
+    """
+    try:
+        from scgen import SCGEN
+    except ModuleNotFoundError as e:
+        raise OptionalDependencyNotInstalled(e)
+
+    scib.utils.check_sanity(adata, batch, hvg)
+
+    net_adata = adata.copy()
+    if hvg is not None:
+        net_adata = net_adata[:, hvg].copy()
+
+    SCGEN.setup_anndata(net_adata, batch_key=batch, labels_key=cell_type)
+    model = SCGEN(net_adata,n_latent = latent_dims)  # adapted n_latent for consitency with other methods
+    model.train(
+        max_epochs=epochs,
+        batch_size=32,
+        early_stopping=True,
+        early_stopping_patience=25,
+    )
+    corrected_adata = model.batch_removal(**kwargs)
+    return corrected_adata
+  
+
+def scanorama(adata, batch, hvg = None,latent_dims = 30, **kwargs):
+    """Scanorama wrapper function
+    Based on `scanorama <https://github.com/brianhie/scanorama>`_ version 1.7.0
+    with adapted default latent space size
+    :param adata: preprocessed ``anndata`` object
+    :param batch: batch key in ``adata.obs``
+    :param hvg: list of highly variables to subset to. If ``None``, the full dataset will be used
+    :return: ``anndata`` object containing the corrected feature matrix as well as an embedding representation of the
+        corrected data
+    """
+    try:
+        import scanorama
+    except ModuleNotFoundError as e:
+        raise OptionalDependencyNotInstalled(e)
+
+    scib.utils.check_sanity(adata, batch, hvg)
+    split, categories = scib.utils.split_batches(adata.copy(), batch, return_categories=True)
+    corrected = scanorama.correct_scanpy(split, return_dimred=True, dimred = latent_dims, **kwargs) # adapted dimred for consitency with other methods
+    corrected = scib.utils.merge_adata(
+        *corrected, batch_key=batch, batch_categories=categories, index_unique=None
+    )
+    corrected.obsm["X_emb"] = corrected.obsm["X_scanorama"]
+    # corrected.uns['emb']=True
+
+    return corrected
+
 
 def runIntegration(inPath, outPath, method, hvg, batch, celltype=None):
     """
@@ -46,10 +106,10 @@ if __name__ == '__main__':
     celltype = args.celltype
     method = args.method
     methods = {
-        'scanorama': scib.integration.scanorama,
+        'scanorama': scanorama,
         'trvae': scib.integration.trvae,
         'trvaep': scib.integration.trvaep,
-        'scgen': scib.integration.scgen,
+        'scgen': scgen,
         'mnn': scib.integration.mnn,
         'bbknn': scib.integration.bbknn,
         'scvi': scib.integration.scvi,
